ce6252e928e513ee15deb708593cd4e098ead9c8
Favour James <favour.ujames196@gmail.com>
2025-01-03 13:29:31 +0100

    tbuilding package

--- textToKnowledgeGraph/__init__.py
+++ textToKnowledgeGraph/__init__.py
@@ -1,12 +1,12 @@
 from .main import process_paper, main
-from .get_interactions import bel_extraction_chain
+from .get_interactions import initialize_chains
 from .sentence_level_extraction import llm_bel_processing
 from .convert_to_cx2 import convert_to_cx2
 
 __all__ = [
     "process_paper",
     "main",
-    "bel_extraction_chain",
+    "initialize_chains",
     "llm_bel_processing",
     "convert_to_cx2",
 ]
